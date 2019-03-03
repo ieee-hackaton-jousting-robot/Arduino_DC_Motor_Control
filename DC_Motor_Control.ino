327f62c6c7d5c29c337e05aa648c1618659da7df
Matthew McGinn <matthew.mcginn96@gmail.com>
2019-03-03 14:29:40 -0500

    Add files via upload

--- DC_Motor_Control.ino
+++ DC_Motor_Control.ino
@@ -1,13 +1,17 @@
 // Define the pin numbers
+#include<PacketSerial.h>
+
+
+PacketSerial myPacketSerial;
 int motorPin1 = 6;
 int motorPin2 = 7;
 int motor2Pin1 = 3;
 int motor2Pin2 = 4;
 int pwmMotor1 = 5;
 int pwmMotor2 = 9;
-int parameter1 = 0;
-int parameter2 = 0;
-int sRate = 9600;
+int8_t parameter1 = 0;
+int8_t parameter2 = 0;
+#define sRate 115200
 
 /*
  * 
@@ -24,24 +28,21 @@ int sRate = 9600;
  * 
  * 
  */
-
 void leftMotor(int input) {
   int dtyCycl = 0;
   if (input < 0) { //move backwards
     digitalWrite(motorPin1, HIGH);
     digitalWrite(motorPin2, LOW);
-  } 
+  }
   else { //move forwards
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, HIGH);
   }
 
   // motor power here
-  dtyCycle = abs(input*2 - 1);
+  dtyCycl = abs(input*2);
   analogWrite(pwmMotor1, dtyCycl);
 }
-
-
 
 void rightMotor(int input) { 
   int dtyCycl = 0;
@@ -55,13 +56,22 @@ void rightMotor(int input) {
   }
 
   // motor power here
-  dtyCycl = abs(input*2 - 1);
+  dtyCycl = abs(input*2);
   analogWrite(pwmMotor2, dtyCycl);
 }
 
-
-
-
+void onPacketReceived(const uint8_t* buffer, size_t size){ //what do we want to do with packet info - simply just store it in 2 parameters
+  //basically read the first and second values of the array
+  if (size < 2){ //this is garbage, do nothing
+    leftMotor(0);
+    rightMotor(0);
+    return;
+  }  
+  parameter1 = buffer[0];
+  parameter2 = buffer[1];
+  leftMotor(parameter1);
+  rightMotor(parameter2);
+}
 
 void setup() {
     // Set the pin modes of the above IO pins to OUTPUT
@@ -70,16 +80,18 @@ void setup() {
     pinMode(motor2Pin1, OUTPUT);
     pinMode(motor2Pin2, OUTPUT);
     pinMode(pwmMotor1, OUTPUT);
-    pinMode(pwnMotor2, OUTPUT);
-    Serial.begin(sRate);
+    pinMode(pwmMotor2, OUTPUT);
+    myPacketSerial.begin(sRate);
+    myPacketSerial.setPacketHandler(&onPacketReceived);
+
 }
 
 void loop() {
+//leftMotor(127);
+//rightMotor(127); 
+  
   //first we need to read the values from serial, take these values as parameters 1 and 2 and use it to control our robot
-  parameter1 = read();
-  parameter2 = read();
-  leftMotor(parameter1);
-  rightMotor(parameter2);
+  myPacketSerial.update(); //regularly needs to do this
   
 //    analogWrite(pwmMotor1, dtyCycl1);
 //    analogWrite(pwmMotor2, dtyCycl2);
